2df5d73f786b4a094576a76c306fa7d84580b280
Nick DeMayo <sunmorgus@gmail.com>
2017-05-21 22:32:03 -0400

    find more answers and times it

--- index.js
+++ index.js
@@ -5,6 +5,15 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
+var calcTime = function(s, e){
+	var timeDiff = e - s;
+	timeDiff /= 1000;
+	//var seconds = Math.round(timeDiff % 60);
+	
+	//console.log("Took " + timeDiff + " seconds");
+	return timeDiff;
+}
+
 var getInput = function(){
 		rl.question('Input Number: ', (answer) => {
 		var inNum = parseInt(answer);
@@ -14,45 +23,165 @@ var getInput = function(){
 		}
 		
 		rl.close();
+		
+		getSeqSumFor(inNum);
+		
 		getSeqSum(inNum);
+		
+		getSeqSumForDown(inNum);
+		
+		getSeqSumDown(inNum);		
 	});
 }
 
 var getSeqSum = function(inNum){
+	var s = new Date();
 	var hold = [];
-	var tries = [];
-	var start = 0;
-	var i = 0;
+	var ans = 0;
+	var start = 1;
+	var i = 1;
 	var found = false;
 	do {
 		hold.push(i);
-		var sum = hold.reduce((a, b) => a + b, 0).toFixed(2);
+		var sum = hold.reduce((a, b) => a + b, 0);
 
-		if (sum === inNum) {
-			console.log('found it');
-			console.log(hold.join('+'));
-			found = true;
+		if (sum === inNum && start != inNum) {
+			ans += 1;
 		}
 		else if (sum < inNum) {
 			i += 1;
 		}
 		else if (sum > inNum) {
-			console.log("Tried starting at " + start);
+			//console.log("Tried starting at " + start);
 			start += 1;
 			i = start;
-			tries.push(hold);
 			hold = [];
 		}
 		
 		if (start > inNum) {
-			//console.log("We tried all these and couldnt get an answer:");
-			//for (var a = 0; a < tries.length; a++) {
-			//	console.log(tries[a].join("+"));
-			//}
-			console.log(inNum + " has no sequential sum!");
 			found = true;
 		}
 	} while (!found);
+
+	var e = new Date();
+	var t = calcTime(s, e);
+	console.log("RedUp found " + ans + " answer(s). Took " + t + " seconds.");
 }
+
+var getSeqSumDown = function(inNum){
+	var s = new Date();
+	var hold = [0];
+	var ans = 0;
+	var start = inNum;
+	var i = inNum - 1;
+	var found = false;
+	do {
+		hold.push(i);
+		var sum = hold.reduce((a, b) => a + b, 0);
+
+		if (sum === inNum) {
+			ans += 1;
+		}
+		else if (sum < inNum) {
+			i -= 1;
+		}
+		else if (sum > inNum) {
+			//console.log("Tried starting at " + start);
+			start -= 1;
+			i = start;
+			
+			hold = [0];
+		}
+		
+		if (start < 0 || i < 0) {
+			found = true;
+		}
+	} while (!found);
+
+	hold = hold.slice(1, hold.length);
+	var e = new Date();
+	var t = calcTime(s, e);			
+	console.log("RedDown found " + ans + " answer(s). Took " + t + " seconds.");
+}
+
+var getSeqSumFor = function(inNum){
+	var s = new Date();
+	var hold = [];
+	var ans = 0;
+	var start = 1;
+	var i = 1;
+	var found = false;
+	do {
+		hold.push(i);
+
+		var sum = 0;
+		for (var b = 0; b < hold.length; b++) {
+			sum += hold[b];
+		}
+		
+		if (sum === inNum && start != inNum) {
+			ans += 1;
+		}
+		else if (sum < inNum) {
+			i += 1;
+		}
+		else if (sum > inNum) {
+			//console.log("Tried starting at " + start);
+			start += 1;
+			i = start;
+			
+			hold = [];
+		}
+		
+		if (start > inNum) {
+			found = true;
+		}
+	} while (!found);
+
+	var e = new Date();
+	var t = calcTime(s, e);		
+	console.log("ForUp found " + ans + " answer(s). Took " + t + " seconds.");
+}
+
+
+var getSeqSumForDown = function(inNum){
+	var s = new Date();
+	var hold = [0];
+	var ans = 0;
+	var start = inNum;
+	var i = inNum - 1;
+	var found = false;
+	do {
+		hold.push(i);
+
+		var sum = 0;
+		for (var b = 0; b < hold.length; b++) {
+			sum += hold[b];
+		}
+		if (sum === inNum) {
+			ans += 1;
+		}
+		else if (sum < inNum) {
+			i -= 1;
+		}
+		else if (sum > inNum) {
+			//console.log("Tried starting at " + start);
+			start -= 1;
+			i = start;
+			
+			hold = [0];
+		}
+		
+		if (start < 0 || i < 0) {
+			found = true;
+		}
+	} while (!found);
+		
+	hold = hold.slice(1, hold.length);
+	var e = new Date();
+	var t = calcTime(s, e);
+	console.log("ForDown found " + ans + " answer(s). Took " + t + " seconds.");
+}
+
 
 getInput();
